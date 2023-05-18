2e94e878ad16bc070464040f76acdea0824ae416
maruti-jogdand <gcpmaruti@gmail.com>
2023-05-18 13:48:22 +0530

    Initial commit

--- Storage/variables.tf
+++ Storage/variables.tf
@@ -1,9 +1,9 @@
 variable "bucket_name" {
   description = "Specify GCS Bucket name"
-  default = "unique-gcs-2132"
+  default     = "unique-gcs-2132"
 }
 
 variable "bucket_location" {
   description = "Specify storage bucket location"
-  default = "US"
+  default     = "US"
 }
