8900ffe8199139a45b493d81b63d06e34dd020af
maruti-jogdand <gcpmaruti@gmail.com>
2023-05-25 19:34:07 +0530

    updated storage tf files

--- Storage/main.tf
+++ Storage/main.tf
@@ -5,10 +5,10 @@ resource "google_storage_bucket" "demo" {
 
   public_access_prevention = "enforced"
 
-  uniform_bucket_level_access = true
+  uniform_bucket_level_access = false
 
   retention_policy {
     retention_period = "315360000"
-    is_locked = true
+    is_locked = false
   }
 }
