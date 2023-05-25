c3a324b5a9830a98adbf378b85697aed6e1c6c30
maruti-jogdand <gcpmaruti@gmail.com>
2023-05-25 19:00:58 +0530

    updated cloudbuild.yaml file updated

--- Storage/main.tf
+++ Storage/main.tf
@@ -4,13 +4,9 @@ resource "google_storage_bucket" "auto-expire" {
   force_destroy = true
   storage_class = "STANDARD"
 
-  versioning {
-    enabled = false
-  }
-
   retention_policy {
     retention_period = "315360000"
-    is_locked = true
+    is_locked = false
   }
 
   public_access_prevention = "enforced"
