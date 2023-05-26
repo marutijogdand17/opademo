a6e4395b29bf743a333d87820e1fb5c38d35b465
maruti-jogdand <gcpmaruti@gmail.com>
2023-05-26 13:26:30 +0530

    updated tf file

--- Storage/main.tf
+++ Storage/main.tf
@@ -1,14 +1,15 @@
 resource "google_storage_bucket" "demo" {
-  name          = "no-public-access-bucket"
+  name          = "wiz-iac-demo-bucket-00"
   location      = "US"
   force_destroy = true
+  project = "wiz-environment-692908"
 
   public_access_prevention = "enforced"
 
-  uniform_bucket_level_access = false
+  uniform_bucket_level_access = true
 
   retention_policy {
     retention_period = "315360000"
-    is_locked = false
+    is_locked = true
   }
 }
